3a180b0093fc6cf47a864524e25a7ba4746fdc9e
SoggyCashew <34151610+oxicottin@users.noreply.github.com>
2021-12-29 00:27:00 -0500

    Add files via upload

--- Libraries/Soil Moisture Sensor/Plant.py
+++ Libraries/Soil Moisture Sensor/Plant.py
@@ -24,9 +24,17 @@ def percentage_value(value, in_min, out_max, out_min):
 while True:
     
 # https://www.calculatorsoup.com/calculators/statistics/average.php
-#    print(adc0.read_u16())  # Run to get wet and dry definition numbers
+#    print(adc0.read_u16())  # Run to get wet and dry variable numbers
 
-    print(percentage_value(adc0.read_u16(), dry, wet, dry))
+#     print(percentage_value(adc0.read_u16(), dry, wet, dry))
+#     utime.sleep(0.5)
+
+    final_value = percentage_value(adc0.read_u16(), dry, wet, dry)
+
+    if final_value >= "50":
+        print(final_value, 'Wet')
+    elif final_value <= "49" and final_value >= "25":
+        print(final_value, 'Moist')
+    elif final_value <= "24":
+        print(final_value, 'Water Me!')
     utime.sleep(0.5)
-
-    
